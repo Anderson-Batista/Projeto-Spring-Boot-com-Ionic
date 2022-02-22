aeda59b54e030d9cf89ab7ac271e7e574fc3320d
Anderson-Batista <andersonbfs15@gmail.com>
2022-02-22 08:37:05 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/andersonbatista/cursomc/services/CategoriaService.java
+++ src/main/java/com/andersonbatista/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.andersonbatista.cursomc.domain.Categoria;
 import com.andersonbatista.cursomc.repositories.CategoriaRepository;
+import com.andersonbatista.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
