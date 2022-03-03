775891a631aadd8427d3a0a5bafc81a9011b654b
Anderson-Batista <andersonbfs15@gmail.com>
2022-03-03 09:41:24 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/andersonbatista/cursomc/services/CategoriaService.java
+++ src/main/java/com/andersonbatista/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.andersonbatista.cursomc.domain.Categoria;
+import com.andersonbatista.cursomc.dto.CategoriaDTO;
 import com.andersonbatista.cursomc.repositories.CategoriaRepository;
 import com.andersonbatista.cursomc.services.exceptions.DataIntegrityException;
 import com.andersonbatista.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
