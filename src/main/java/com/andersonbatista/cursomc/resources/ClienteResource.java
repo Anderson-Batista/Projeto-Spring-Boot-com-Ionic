6631e0d9c497c995ae7ad97a137e573954986f9c
Anderson-Batista <andersonbfs15@gmail.com>
2022-03-09 09:01:22 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/andersonbatista/cursomc/resources/ClienteResource.java
+++ src/main/java/com/andersonbatista/cursomc/resources/ClienteResource.java
@@ -17,9 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.andersonbatista.cursomc.domain.Categoria;
 import com.andersonbatista.cursomc.domain.Cliente;
-import com.andersonbatista.cursomc.dto.CategoriaDTO;
 import com.andersonbatista.cursomc.dto.ClienteDTO;
 import com.andersonbatista.cursomc.dto.ClienteNewDTO;
 import com.andersonbatista.cursomc.services.ClienteService;
